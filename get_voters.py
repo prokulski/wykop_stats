f3f9b056992d68f35fcf4b1b9bd22191d999946b
Łuaksz Prokulski <prokulski@gmail.com>
2019-11-30 17:34:33 +0100

    poprawka błędu przy zerowej liczbie zakopów

--- get_voters.py
+++ get_voters.py
@@ -8,7 +8,7 @@ import sqlite3
 
 import logging
 
-logging.basicConfig(level=logging.WARNING, format='%(asctime)s :: %(levelname)s :: %(filename)s :: %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s :: %(levelname)s :: %(filename)s :: %(message)s')
 
 # wykorzystujemy metody z pakietu
 from grabber import get_wykop_upvoters,get_wykop_downvoters
@@ -45,29 +45,31 @@ df_len = len(df)
 for r in range(df_len):
     print(f"{r} of {df_len}")
 
-    logging.info(
-        "Pobieram upvoters dla {a} wiersza z {b} (znalezisko ID={c})".format(a=r, b=df_len, c=df.iloc[r]['id']))
+    logging.info("Pobieram upvoters dla {a} wiersza z {b} (znalezisko ID={c})".format(a=r, b=df_len, c=df.iloc[r]['id']))
     # pobieramy listę upvoters dla konkternego ID znaleziska
     upvoters_org = get_wykop_upvoters(df.iloc[r]['id'])
+    # formalnie powinniśmy sprawdzić czy są wykopywacze,
+    # ale bez nich Wykop.pl nie miałby sensu
+    # zatem olewamy :)
     upvoters = pd.DataFrame(upvoters_org)
     # wyciągamy login wykopującego
     upvoters['upvoter'] = upvoters['author'].apply(lambda x: x['login'])
     upvoters['id'] = df.iloc[r]['id']
     upvoters = upvoters[['id', 'upvoter', 'date']]
+    logging.info("Zapisuję do bazy info o upvoters")
+    upvoters.to_sql("upvoters", db_conn, if_exists="append", index=False)
 
     # to samo dla downvoters
-    logging.info(
-        "Pobieram downvoters dla {a} wiersza z {b} (znalezisko ID={c})".format(a=r, b=df_len, c=df.iloc[r]['id']))
+    logging.info("Pobieram downvoters dla {a} wiersza z {b} (znalezisko ID={c})".format(a=r, b=df_len, c=df.iloc[r]['id']))
     downvoters_org = get_wykop_downvoters(df.iloc[r]['id'])
-    downvoters = pd.DataFrame(downvoters_org)
-    downvoters['downvoter'] = downvoters['author'].apply(lambda x: x['login'])
-    downvoters['id'] = df.iloc[r]['id']
-    downvoters = downvoters[['id', 'downvoter', 'date', 'reason']]
-
-    # dodanie zebranych danych do bazy w SQL
-    logging.info("Zapisuję do bazy info o upvoters i downvoters")
-    upvoters.to_sql("upvoters", db_conn, if_exists="append", index=False)
-    downvoters.to_sql("downvoters", db_conn, if_exists="append", index=False)
+    # sprawdzamy czy byli jacyś zakopywacze
+    if len(downvoters_org) > 0:
+        downvoters = pd.DataFrame(downvoters_org)
+        downvoters['downvoter'] = downvoters['author'].apply(lambda x: x['login'])
+        downvoters['id'] = df.iloc[r]['id']
+        downvoters = downvoters[['id', 'downvoter', 'date', 'reason']]
+        logging.info("Zapisuję do bazy info o downvoters")
+        downvoters.to_sql("downvoters", db_conn, if_exists="append", index=False)
 
 # teraz można zamknąć bazę
 db_conn.close()
